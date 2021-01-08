7284553341f940b76a68277a61b090ad6b6f1dcc
jordannadroj <jordansiemens93@gmail.com>
2021-01-08 15:15:58 +0100

    added testing

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -3,20 +3,32 @@ require "application_system_test_case"
 class QuestionsTest < ApplicationSystemTestCase
   # test "visiting the index" do
   #   visit questions_url
-
+  #
   #   assert_selector "h1", text: "Questions"
   # end
   test "visiting /ask renders the form" do
     visit ask_url
     assert_selector "p", text: "Ask your coach anything"
+  end
+
+   test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+    # to take a screeshot of the result, code below
+    # take_screenshot
+  end
+
+  test "askng a question ending with questions mark yields \"Silly question, get dressed and go to work!\"" do
+    visit ask_url
+    fill_in "question", with: "What is your name?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
     take_screenshot
   end
 
-  # test "saying Hello yields a grumpy response from the coach" do
-  #   visit ask_url
-  #   fill_in "question", with: "Hello"
-  #   click_on "Ask"
 
-  #   assert_text "I don't care, get dressed and go to work!"
-  # end
 end
